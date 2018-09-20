4f31aec9ab7c1b78520da30b3a2b3f25da2c032a
Tsvetan Georgiev <tsvetan.georgiev@chaindigit.com>
2018-09-19 20:04:23 -0400

    Docker

---
+++ src/main/java/com/example/client/JavaSDKApplication.java
@@ -0,0 +1,11 @@
+package com.example.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSDKApplication {
+	public static void main(String args[]) {
+		SpringApplication.run(JavaSDKApplication.class, args);
+	}
+}
